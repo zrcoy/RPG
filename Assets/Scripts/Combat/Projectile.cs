858ba6d3e3a46ebd105cb398c42f7b39ec711e25
Ruichao Zhang <zhangruichaotyler@gmail.com>
2020-10-24 15:30:17 -0400

    Fireball, iceball projectile/pickups added

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -9,8 +9,18 @@ namespace RPG.Combat
     public class Projectile : MonoBehaviour
     {
         [SerializeField] float speed = 2;
+        [SerializeField] bool isHoming = false;
+
+        const float TimeMaxExisting = 2f;
+
         Health target = null;
         float damage = 0;
+        float timerForExist = 0;
+
+        private void Start()
+        {
+            transform.LookAt(GetAimLocation());
+        }
 
         void Update()
         {
@@ -18,9 +28,16 @@ namespace RPG.Combat
             {
                 return;
             }
-
-            transform.LookAt(GetAimLocation());
+            if (isHoming && !target.IsDead())
+            {
+                transform.LookAt(GetAimLocation());
+            }
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            timerForExist += Time.deltaTime;
+            if (timerForExist >= TimeMaxExisting)
+            {
+                Destroy(gameObject);
+            }
         }
 
         public void SetTarget(Health target, float damage)
@@ -42,7 +59,11 @@ namespace RPG.Combat
 
         void OnTriggerEnter(Collider other)
         {
-            if(other.GetComponent<Health>() != target)
+            if (other.GetComponent<Health>() != target)
+            {
+                return;
+            }
+            if (other.GetComponent<Health>().IsDead())
             {
                 return;
             }
